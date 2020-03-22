5336d178e74fcfda213fc671c08c3a3684aa5b91
Claudio Gonçalves <claudiofilipesilvagoncalves@gmail.com>
2020-03-22 22:31:22 +0100

    Working with the binary tree. Still with bugs

--- manageTasks/taskManager/binarySearchTree.py
+++ manageTasks/taskManager/binarySearchTree.py
@@ -3,9 +3,15 @@ class BinarySearchTree:
     class Node:
 
         def __init__(self,task,leftNode,rightNode):
-            self.task = task
+            self.taskArray = []
             self.leftNode = None
             self.rightNode = None
+
+        def __str__(self):
+            taskArray = []
+            for task in self.taskArray :
+                taskArray = taskArray + "\n" + str(task)
+            return taskArray
 
     def __init__(self):
         self.firstNode = None
@@ -19,15 +25,22 @@ class BinarySearchTree:
             selectionInsert(node,tree)
 
         def selectionInsert(node,tree):
-            if tree.priority > node.priority and tree.leftNode == None:
+            if len(tree.taskArray[0]) > 0 and tree.taskArray[0].priority > node.priority:
                 print("Add node right when there are no nodes")
-                tree.leftNode = node
-            elif tree.priority < node.priority and tree.rightNode == None:
-                print("Add Node left when there are no nodes")
-                tree.leftNode = node
-            elif tree.priority == node.priority:
-                print("Add node whenn you find the right priority")
-            elif tree.priority => node.priority:
                 return transverseTree(node,tree.leftNode)
-            elif tree.priority < node.priority:
+            elif len(tree.taskArray[0]) > 0 and tree.priority < node.priority :
+                print("Add Node left when there are no nodes")
                 return transverseTree(node,tree.rightNode)
+            elif len(tree.taskArray[0]) == 0 and tree.priority == node.priority:
+                tree.taskArray.append(node)
+                print("Add node when you find the right priority")
+
+    def printTree(self):
+        tree = self.firstNode
+        transverseTree(tree)
+
+        def transverseTree(tree):
+            transverseTree(tree.leftNode)
+            if len(tree.task) > 1:
+                print(tree.task)
+            transverseTree(tree.rightNode)
--- manageTasks/taskManager/taskmanager.py
+++ manageTasks/taskManager/taskmanager.py
@@ -1,17 +1,18 @@
+from binarySearchTree import BinarySearchTree
+
 class TaskManager(object):
     """docstring forTaskManager."""
 
     def __init__(self):
+        self.binarySearchTree = BinarySearchTree()
         print("Initing TaskManager")
         print("Initializing the BinaryBalanceTree")
 
     """Adding task ordered by priority"""
     """Maibe adding a check for autheticity of the object """
     def addTask(self,task):
-        print("Adding task to the BBT")
+        self.binarySearchTree.addNode(task)
+        print("Task added with success")
 
     def __str__(self):
-        taskString = ""
-        for task in self.taskArray:
-            taskString = taskString + str(task) + " \n"
-        return taskString
+        print(self.binarySearchTree.printTree())
